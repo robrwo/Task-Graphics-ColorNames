5ae0c0adf3191cdeebf73cfbb824df4f3e37c3c3
Robert Rothenberg <rrwo@cpan.org>
2018-09-30 15:33:06 +0100

    Add stub module

---
+++ lib/Task/Graphics/ColorNames.pm
@@ -0,0 +1,32 @@
+package Task::Graphics::ColorNames;
+
+# ABSTRACT: Install Graphics::ColorNames and extra colour schemes
+
+use v5.8;
+
+use strict;
+use warnings;
+
+our $VERSION = 'v2.0.0';
+
+use Graphics::ColorNames ();
+
+use Graphics::ColorNames::CSS;
+use Graphics::ColorNames::Crayola;
+use Graphics::ColorNames::EmergyC;
+use Graphics::ColorNames::GrayScale;
+use Graphics::ColorNames::IE;
+use Graphics::ColorNames::Mozilla;
+use Graphics::ColorNames::Netscape;
+use Graphics::ColorNames::SVG;
+use Graphics::ColorNames::VACCC;
+use Graphics::ColorNames::WWW;
+use Graphics::ColorNames::Werner;
+
+=head1 SEE ALSO
+
+This module supercedes C<Bundle::Graphics::ColorNames>.
+
+=cut
+
+1;
